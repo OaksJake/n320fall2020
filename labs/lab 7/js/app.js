6306c8b86aa5b3d4e07b916bc01cc7ea50e4313c
Jake <35298312+OaksJake@users.noreply.github.com>
2020-10-31 01:26:59 -0400

    Board game project

---
+++ labs/lab 7/js/app.js
@@ -0,0 +1,4 @@
+
+fetch("data/data.json")
+    .then(response => response.json())
+    .then(data => console.log(data));
---
+++ labs/project 1/js/app.js
@@ -0,0 +1,21 @@
+class ecks {
+
+}
+
+class ooo {
+
+}
+
+var prompter = document.getElementById("prompt");
+prompter.innerHTML = "It is X's turn";
+
+var one = document.getElementById("1");
+one.addEventListener(onclick, );
+var two = document.getElementById("2");
+var three = document.getElementById("3");
+var four = document.getElementById("4");
+var five = document.getElementById("5");
+var six = document.getElementById("6");
+var seven = document.getElementById("7");
+var eight = document.getElementById("8");
+var nine = document.getElementById("9");
