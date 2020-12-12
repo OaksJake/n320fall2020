785d8fbad3c2b115c524cc7282468e3e51bfdde7
Jake <35298312+OaksJake@users.noreply.github.com>
2020-12-12 14:16:13 -0500

    final

---
+++ Color Mixer/random/js/app.js
@@ -0,0 +1,7 @@
+var names = ["Jake", "Emersyn", "Aviante", "Moose", "Andrew", "Dylan", "Conner"];
+var name = document.getElementById("press");
+name.addEventListener("click", onClick);
+
+function click() {
+  consol.log("hi")
+}
